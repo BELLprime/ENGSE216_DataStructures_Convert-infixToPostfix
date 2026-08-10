1a5b873062c1deac6f98b80acb29e216799bfe63
BELLprime <natthakitrodruean@gmail.com>
2026-08-10 16:04:46 +0700

    "refactor: Refactoring code"

--- src/conversion_infixtopostfix/Stack.java
+++ src/conversion_infixtopostfix/Stack.java
@@ -2,36 +2,29 @@ package conversion_infixtopostfix;
 import java.util.EmptyStackException;
 
 public class Stack {
-    private Node head,top,temp;
+    private Node top,temp;
     private int count;
 
     public Stack() {
-        this.top=this.head=null;
+        this.top=null;
         this.count=0;
     }
     //push
     public void push(char item) {
-        Node nn =new Node(item);
-        if (isEmpty()) {head=top=nn;}
-        else {
-            nn.link=top;
-            top=nn;
-        }
+        Node nn=new Node(item);
+        nn.link=top;      
+        top=nn;
         count++;
     }
     //pop
     public char pop() {
-        if(!isEmpty()) {
-            temp=top;  
-            if (top.link==null) {
-                top=head=null;
-            } else {
-                top=top.link;
-                temp.link=null;
-            }
+        if (!isEmpty()) {
+            temp=top;
+            top=top.link;
+            temp.link=null;
             count--;
             return temp.info;
-        } 
+        }
         throw new EmptyStackException();
     }
     //chcek
